aa6d2994d9c98d7a5ce787a7668053672ab6c008
FeliciaMarlove <missflorencemary@hotmail.com>
2020-05-21 09:58:15 +0200

    Task create

--- src/app/Task/TaskCrud/task-create/task-create.component.ts
+++ src/app/Task/TaskCrud/task-create/task-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {TaskService} from '../../../_Services/task-service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-task-create',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-create.component.scss']
 })
 export class TaskCreateComponent implements OnInit {
+  private form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private taskService: TaskService,
+    private fb: FormBuilder,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      taskName : ['', Validators.required],
+      taskShortDescription : ['', Validators.required],
+      taskLongDescription : ['']
+    });
   }
 
+  onCreate() {
+    this.taskService.createTask(this.form.value).subscribe( response => {
+      if (!response.taskId) {
+        window.alert(response.msg);
+      } else {
+        this.form.reset();
+      }
+    });
+  }
+
+  onCancel() {
+    if (!this.form.pristine) {
+      if (confirm('Les modifications dans le formulaire vont être perdues, continuer ?')) {
+        this.router.navigateByUrl('/dashboard/task/read');
+      }
+    } else {
+      this.router.navigateByUrl('/dashboard/task/read');
+    }
+  }
 }
