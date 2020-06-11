0d9c9198d8fdbcce2aca2def634f786e2472d430
FeliciaMarlove <missflorencemary@hotmail.com>
2020-06-11 21:18:25 +0200

    A bit more documentation

--- src/app/_Services/connection-service.ts
+++ src/app/_Services/connection-service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {User} from '../_Models/user';
 import {Observable} from 'rxjs';
@@ -20,11 +20,13 @@ export class ConnectionService {
   }
 
   /**
-   * Tente une connexion à l'application et enregistre le code Basic 64 dans une variable de session
+   * Tente une connexion à l'application et enregistre le code Basic 64 dans une variable de session.
+   * Définit le code Basic 64.
+   * Retourne un Observable de type User.
    * @param user l'utilisateur (e-mail et mot de passe)
    */
   public connect(user: User): Observable<User> {
-  sessionStorage.setItem('auth', btoa(user.email + ':' + user.password));
-  return this.http.post<User>(URI + 'connect', user);
+    sessionStorage.setItem('auth', btoa(user.email + ':' + user.password));
+    return this.http.post<User>(URI + 'connect', user);
   }
 }
